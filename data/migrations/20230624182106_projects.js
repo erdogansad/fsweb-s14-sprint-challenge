f2e6b2306501bc24bb8fedbc6528a300737d6459
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-24 21:32:21 +0300

    migration created

---
+++ data/migrations/20230624182106_projects.js
@@ -0,0 +1,30 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name").notNullable();
+      tbl.string("project_description");
+      tbl.boolean("project_completed").defaultTo(0);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name").unique().notNullable();
+      tbl.string("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description").notNullable();
+      tbl.string("task_notes");
+      tbl.boolean("task_completed").defaultTo(0);
+      tbl.integer("project_id").references("projects").inTable("project_id");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments();
+      tbl.integer("project_id").references("projects").inTable("project_id");
+      tbl.integer("resource_id").references("resources").inTable("resource_id");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("icindekiler");
+};
