73dc61dc29f96fc973048d54dd6edda8c01d20c0
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-24 21:32:14 +0300

    server created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// serverı buraya yazın ve index.js require yazın
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const { errHandler } = require("./server-middleware.js");
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(morgan("combined"));
+server.set("json spaces", 2);
+
+server.use(errHandler);
+
+module.exports = server;
